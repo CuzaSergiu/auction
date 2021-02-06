2b2efa4c1463317277261fa480471a51cef1400c
Cuza Sergiu <cuza.sergiu@yahoo.com>
2021-02-06 12:13:43 +0200

    initial project draft

---
+++ src/main/java/com/sda/auction/config/Config.java
@@ -0,0 +1,23 @@
+package com.sda.auction.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class Config {
+
+
+    // this bean will set the path to our html from templates
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML");
+        resolver.setCacheable(false);
+        return resolver;
+    }
+
+}
