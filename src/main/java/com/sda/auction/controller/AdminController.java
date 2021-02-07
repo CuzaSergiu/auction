014580712fa48ad2cec7de233754c171e2b1120e
Cuza Sergiu <cuza.sergiu@yahoo.com>
2021-02-07 14:29:23 +0200

    css added

---
+++ src/main/java/com/sda/auction/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.sda.auction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    // == methods ==
+    @GetMapping("/addProduct")
+    public String getAddProduct() {
+        return "addProduct";
+    }
+
+}
